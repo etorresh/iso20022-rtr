491b522603b7de3bc91283d6c8dfe104fae62ba0
etorresh <emiliotorreshinojosa@gmail.com>
2026-08-16 04:54:26 -0600

    add schema code generation pipeline and generate schemas

--- tests/roundtrip.rs
+++ tests/roundtrip.rs
@@ -13,7 +13,7 @@ pub fn assert_roundtrip(file_path: &str, is_expected_variant: impl FnOnce(&Messa
     assert!(is_expected_variant(&parsed_struct.payload));
 }
 
-// rust-analyzer hates include_str! https://github.com/rust-lang/rust-analyzer/issues/10647 so I'm reading the file at run-time
+// rust-analyzer hates include_str! https://github.com/rust-lang/rust-analyzer/issues/10647 so I'm reading the file at run-time and using format!
 macro_rules! test_roundtrip {
     ($folder_name:ident, $req_ty:path, $res_ty:path) => {
         mod $folder_name {
---
+++ tools/codegen/src/main.rs
@@ -0,0 +1,23 @@
+use std::fs;
+use typify::{TypeSpace, TypeSpaceSettings};
+
+// reference: https://github.com/oxidecomputer/typify/blob/main/example-build/build.rs
+fn main() {
+    let schemas_dir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("../../schemas/");
+    let output_dir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("../../src/generated/");
+    let entries = std::fs::read_dir(schemas_dir).unwrap();
+    for schema_path in entries.map(|entry| entry.unwrap().path()) {
+        let content = fs::read_to_string(&schema_path).unwrap();
+        let schema = serde_json::from_str::<schemars::schema::RootSchema>(&content).unwrap();
+
+        let mut type_space = TypeSpace::new(TypeSpaceSettings::default().with_struct_builder(true));
+        type_space.add_root_schema(schema).unwrap();
+
+        let contents =
+            prettyplease::unparse(&syn::parse2::<syn::File>(type_space.to_stream()).unwrap());
+
+        let output_filename = schema_path.file_name().unwrap();
+        let output_path = output_dir.join(output_filename);
+        fs::write(output_path, contents).unwrap();
+    }
+}
